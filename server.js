f9a3de33bc92e0d5d22df859e8f4634d08ed055c
sarahhyy <sarahh.yy@gmail.com>
2015-04-12 11:03:33 -0400

    all final

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://qchat:qchat@ds061641.mongolab.com:61641/qchat');
 
 app.use(express.static('public'));
 //app.use('/public/assets', express.static(__dirname + '/public/assets'));
+app.set('view engine', 'jade');
 
 
 var Schema = mongoose.Schema;
@@ -26,11 +27,21 @@ var userSchema = new Schema({
    // keywords : [String]
 });
 var qmodel = mongoose.model('question',userSchema);
+var qmodel2 = mongoose.model('question2',userSchema);
 app.get('/questions', function (req,res){
    mongoose.model('question').find(function(err,qs){
    	res.send(qs);
    })
 });
+
+app.get('/questions2', function (req, res) {
+
+  mongoose.model('question2').find(function(err,qs){
+        res.send(qs);
+   });
+  
+});
+
 
 io.on('connection', function(socket){
    console.log('user connected');
@@ -56,21 +67,48 @@ io.on('connection', function(socket){
 
     		mongoose.model('question').find(function(err,qs){
    			io.emit('postquestion', JSON.stringify(q));
-   })
+   });
+    });
+    });
+
+        socket.on('askquestion2', function(msg){    
+      console.log('message recieved ');
+      var q = JSON.parse(msg);
+
+      
+        qm2 = new qmodel2();
+        qm2._id = new mongoose.Types.ObjectId();;
+      qm2.question = q.question;
+      qm2.userid = q.userid;
+      qm2.upvotes = q.upvotes;
+      qm2.spam = q.spam;
+      qm2.spamcount = q.spamcount;
+      qm2.postedon = q.postedon;
+      //qm.keywords = q.keywords;
+      q._id = qm2._id;
+      qm2.save(function (err) {
+
+        mongoose.model('question2').find(function(err,qs){
+        io.emit('postquestion2', JSON.stringify(q));
+   });
 
 
   
-});
+
       
       
+    });
+
     });
 
 
     socket.on('upvote', function(msg){
 
-    	qu = new qmodel();
+    	//qu = new qmodel();
 
-    	qu.update({_id:msg},{ $inc: { upvotes: 1}});
+    	qmodel.update({_id:msg},{ $inc: { upvotes: 1}},{multi:true},function  (err, numAffected) {
+  // numAffected is the number of updated documents
+});
     	io.emit('incupvote',msg);
 
     });
